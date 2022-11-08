816962941b383ad27f0f7380a6947761ab7c61ac
puff <33184334+puff@users.noreply.github.com>
2022-11-08 16:06:19 -0500

    Initial commit. Foundations have been laid. Commence suffering.

---
+++ EazyDevirt/Abstractions/ILogger.cs
@@ -0,0 +1,10 @@
+﻿namespace EazyDevirt.Abstractions;
+
+public interface ILogger
+{
+    void Success(string message);
+    void Warning(string message);
+    void Error(string message);
+    void Info(string message);
+    void InfoStr(string message, string message2);
+}
---
+++ EazyDevirt/Abstractions/IStage.cs
@@ -0,0 +1,9 @@
+﻿using EazyDevirt.Devirtualization;
+
+namespace EazyDevirt.Abstractions;
+
+public interface IStage
+{
+    string Name { get; }
+    void Run(DevirtualizationContext ctx);
+}
