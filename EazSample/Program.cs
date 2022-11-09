cdcdbba6dd74e16b66d7c0696ee597b14a2a4670
puff <>
2022-11-09 13:32:42 -0500

    EazSample build event to copy output.

--- EazSample/Program.cs
+++ EazSample/Program.cs
@@ -5,35 +5,35 @@ namespace EazSample
     internal class Program
     {
         [Obfuscation(Feature = "virtualization", Exclude = false)]
-        private static int virtualized_int;
+        private static int _virtualizedInt;
 
         [Obfuscation(Feature = "virtualization", Exclude = false)]
-        private static string virtualized_string;
+        private static string? _virtualizedString;
         
-        private static uint not_virtualized_uint;
+        private static uint _notVirtualizedUint;
         
         [Obfuscation(Feature = "virtualization", Exclude = false)]
         private static void Main(string[] args)
         {
             Console.WriteLine("definitely not a virtualized entrypoint");
 
-            virtualized_int = 1337;
-            not_virtualized_uint = 0xc01db33f; // 3223171903
-            virtualized_string = "100% not a virtualized string";
-            var ret_code = clear_method(virtualized_string);
-            Console.WriteLine("Return code: " + ret_code);
+            _virtualizedInt = 1337;
+            _notVirtualizedUint = 0xc01db33f; // 3223171903
+            _virtualizedString = "100% not a virtualized string";
+            var retCode = ClearMethod(_virtualizedString);
+            Console.WriteLine("Return code: " + retCode);
         }
 
-        private static long clear_method(string arg)
+        private static long ClearMethod(string? arg)
         {
             Console.WriteLine(arg);
-            var ret_code = virtualized_method(virtualized_int, not_virtualized_uint);
-            Console.WriteLine(ret_code % 1337); // 490 0x1EA
-            return ret_code;
+            var retCode = VirtualizedMethod(_virtualizedInt, _notVirtualizedUint);
+            Console.WriteLine(retCode % 1337); // 490 0x1EA
+            return retCode;
         }
 
         [Obfuscation(Feature = "virtualization", Exclude = false)]
-        private static long virtualized_method(int arg1, uint arg2)
+        private static long VirtualizedMethod(int arg1, uint arg2)
         {
             return arg1 + arg2; // 3223173240 0xC01DB878
         }
