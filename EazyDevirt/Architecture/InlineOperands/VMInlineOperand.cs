20ddace7e814015e1f76d5191682b0d9a680f4de
puff <>
2022-12-31 23:59:56 -0500

    Add Resolver.cs for resolving types. Currently has ResolveType(int position);

---
+++ EazyDevirt/Architecture/InlineOperands/VMInlineOperand.cs
@@ -0,0 +1,110 @@
+﻿namespace EazyDevirt.Architecture.InlineOperands;
+
+// thank you to saneki
+
+/// <summary>
+/// A guess as to the first byte (enum) of VMInlineOperand.
+/// </summary>
+internal enum ValueType
+{
+    /// <summary>
+    /// The Value field holds a raw MetadataToken value.
+    /// </summary>
+    Token = 0,
+
+    /// <summary>
+    /// The Value field holds a position.
+    /// </summary>
+    Position = 1
+}
+
+/// <summary>
+/// Inline operand types.
+/// </summary>
+/// <remarks>
+/// TODO: These change across samples.
+/// </remarks>
+internal enum VMInlineOperandType
+{
+    Type = 0,
+    Field = 1,
+    Method = 2,
+    UnknownType = 3,
+    UserString = 4
+}
+
+/// <summary>
+/// Deserialized inline operand.
+/// </summary>
+internal record VMInlineOperand
+{
+    /// <summary>
+    /// Determines how the Value field is interpreted.
+    /// </summary>
+    public ValueType ValueType { get; }
+
+    /// <summary>
+    /// Either a raw metadata token from the parent module, or a position in
+    /// the VM resource stream.
+    /// </summary>
+    public int Value { get; }
+
+    /// <summary>
+    /// Deserialized data associated with this operand.
+    /// </summary>
+    public VMInlineOperandData Data { get; }
+    
+    /// <summary>
+    /// Whether or not this operand contains a token.
+    /// </summary>
+    public bool IsToken => ValueType == ValueType.Token;
+
+    /// <summary>
+    /// Whether or not this operand contains a position.
+    /// </summary>
+    public bool IsPosition => !IsToken;
+    
+    /// <summary>
+    /// Get the operand's token, throwing an exception if none.
+    /// </summary>
+    public int Token => IsToken ? Value : throw new Exception("InlineOperand has no token (only position)");
+
+    /// <summary>
+    /// Get the operand's position, throwing an exception if none.
+    /// </summary>
+    public int Position => IsPosition ? Value : throw new Exception("InlineOperand has no position (only token)");
+
+    /// <summary>
+    /// Whether or not this operand has deserialized data associated with it.
+    /// </summary>
+    public bool HasData => Data != null;
+
+    public VMInlineOperand(ValueType valueType, int value)
+    {
+        ValueType = valueType;
+        Value = value;
+    }
+    
+    public VMInlineOperand(BinaryReader reader)
+    {
+        ValueType = (ValueType)reader.ReadByte();
+
+        if (ValueType == ValueType.Token)
+            Value = reader.ReadInt32();
+        else
+            Data = VMInlineOperandData.Read(reader);
+    }
+
+    public static VMInlineOperand ReadInternal(BinaryReader reader) => new(ValueType.Position, reader.ReadInt32());
+
+    public static VMInlineOperand[] ReadArrayInternal(BinaryReader reader)
+    {
+        var count = reader.Read7BitEncodedInt();
+        var arr = new VMInlineOperand[count];
+
+        for (var i = 0; i < arr.Length; i++)
+            arr[i] = ReadInternal(reader);
+
+        return arr;
+    }
+}
--- EazyDevirt/Devirtualization/Pipeline/MethodDisassembler.cs
+++ EazyDevirt/Devirtualization/Pipeline/MethodDisassembler.cs
@@ -9,12 +9,17 @@ internal class MethodDisassembler : Stage
     private CryptoStreamV3 VMStream { get; set; }
     private VMBinaryReader VMStreamReader { get; set; }
     
+    private Resolver Resolver { get; set; }
+    
     // private int PreviousReadVMOpCode { get; set; }
     
     public override bool Run()
     {
+        if (!Init()) return false;
+        
         VMStream = new CryptoStreamV3(Ctx.VMStream, Ctx.MethodCryptoKey, true);
         VMStreamReader = new VMBinaryReader(VMStream, true);
+        Resolver = new Resolver(Ctx);
         foreach (var vmMethod in Ctx.VMMethods)
         { 
             // if (vmMethod.EncodedMethodKey != @"5<]fEBf\76") continue;
@@ -42,11 +47,29 @@ internal class MethodDisassembler : Stage
         for (var i = 0; i < vmMethod.VMExceptionHandlers.Capacity; i++)
             vmMethod.VMExceptionHandlers.Add(new VMExceptionHandler(VMStreamReader));
         
-        // TODO: Resolve local and parameter types
+        // TODO: may need to add SortVMExceptionHandlers
         
+        var instructionsPosition = VMStream.Position;
+        
+        ResolveLocalsAndParamters(vmMethod);
 
-        
+        VMStream.Seek(instructionsPosition, SeekOrigin.Begin);
         ReadInstructions(vmMethod);
+    }
+
+    private void ResolveLocalsAndParamters(VMMethod vmMethod)
+    {
+        foreach (var local in vmMethod.MethodInfo.VMLocals)
+        {
+            var type = Resolver.ResolveType(local.Type);
+            Console.WriteLine($"[{vmMethod.MethodInfo.Name}] Local: {type.Name}");
+        }
+        
+        foreach (var parameter in vmMethod.MethodInfo.VMParameters)
+        {
+            var type = Resolver.ResolveType(parameter.Type);
+            Console.WriteLine($"[{vmMethod.MethodInfo.Name}] Parameter: {type.Name}");
+        }
     }
 
     private void ReadInstructions(VMMethod vmMethod)
