b5253307edb84afe169fd57b26bb90e8311ae297
puff <33184334+puff@users.noreply.github.com>
2023-01-15 14:18:42 -0500

    Add ldnull opcode, fix ldloca opcodes, lay some more infrastructure.

--- EazyDevirt/PatternMatching/Patterns/OpCodes/Misc.cs
+++ EazyDevirt/PatternMatching/Patterns/OpCodes/Misc.cs
@@ -61,3 +61,31 @@ internal record Ret : IOpCodePattern
 }
 #endregion Return
 
+#region Ldnull
+internal record Ldnull : IOpCodePattern
+{
+    public IList<CilOpCode> Pattern => new List<CilOpCode>
+    {
+        CilOpCodes.Ldarg_0,     // 0	0000	ldarg.0
+        CilOpCodes.Newobj,      // 1	0001	newobj	instance void VMObjectOperand::.ctor()
+        CilOpCodes.Callvirt,    // 2	0006	callvirt	instance void VM::PushStack(class VMOperandType)
+        CilOpCodes.Ret          // 3	000B	ret
+    };
+
+    public CilOpCode CilOpCode => CilOpCodes.Ldnull;
+
+    public bool Verify(VMOpCode vmOpCode, int index)
+    {
+        var instructions = vmOpCode.SerializedDelegateMethod.CilMethodBody!.Instructions;
+        var vmObjectOperandCtor = instructions[1].Operand as SerializedMethodDefinition;
+        if (vmObjectOperandCtor?.Name != ".ctor") return false;
+
+        var declaringType = vmObjectOperandCtor.DeclaringType!;
+        if (declaringType.Fields.Count != 1 ||
+            declaringType.Fields[0].Signature!.FieldType.FullName != "System.Object") return false;
+        
+        var baseType = declaringType.BaseType?.Resolve();
+        return baseType is { Fields.Count: 2 } && baseType.Fields.Count(f => f.Signature!.FieldType.FullName == "System.Int32") == 1 || baseType!.Fields.Count(f => f.Signature!.FieldType.FullName == "System.Type") == 1;
+    }
+}
+#endregion Ldnull
