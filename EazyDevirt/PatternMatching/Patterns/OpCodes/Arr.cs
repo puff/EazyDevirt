cf146f2843bb3c3be03ecac2480884833618a187
puff <>
2023-03-11 20:50:16 -0500

    Match ldlen opcode

--- EazyDevirt/PatternMatching/Patterns/OpCodes/Arr.cs
+++ EazyDevirt/PatternMatching/Patterns/OpCodes/Arr.cs
@@ -34,3 +34,33 @@ internal record Newarr : IOpCodePattern
             FullName: "System.Array System.Array::CreateInstance(System.Type, System.Int32)"
         };
 }
+
+internal record Ldlen : IOpCodePattern
+{
+    public IList<CilOpCode> Pattern => new List<CilOpCode>
+    {
+        CilOpCodes.Ldarg_0,     // 0	0000	ldarg.0
+        CilOpCodes.Callvirt,    // 1	0001	callvirt	instance class VMOperandType VM::PopStack()
+        CilOpCodes.Callvirt,    // 2	0006	callvirt	instance object VMOperandType::vmethod_0()
+        CilOpCodes.Castclass,   // 3	000B	castclass	[mscorlib]System.Array
+        CilOpCodes.Stloc_0,     // 4	0010	stloc.0
+        CilOpCodes.Ldarg_0,     // 5	0011	ldarg.0
+        CilOpCodes.Ldloc_0,     // 6	0012	ldloc.0
+        CilOpCodes.Callvirt,    // 7	0013	callvirt	instance int32 [mscorlib]System.Array::get_Length()
+        CilOpCodes.Newobj,      // 8	0018	newobj	instance void VMIntOperand::.ctor(int32)
+        CilOpCodes.Callvirt,    // 9	001D	callvirt	instance void VM::PushStack(class VMOperandType)
+        CilOpCodes.Ret          // 10	0022	ret
+    };
+
+    public CilOpCode CilOpCode => CilOpCodes.Ldlen;
+
+    public bool InterchangeLdlocOpCodes => true;
+    public bool InterchangeStlocOpCodes => true;
+
+    public bool Verify(VMOpCode vmOpCode, int index) =>
+        vmOpCode.SerializedDelegateMethod.CilMethodBody!.Instructions[index + 7].Operand is SerializedMemberReference
+        {
+            FullName: "System.Int32 System.Array::get_Length()"
+        };
+}
+
