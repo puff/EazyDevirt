008fd4a87b9cc02b2c23d9c3a3fa448db571b843
puff <33184334+puff@users.noreply.github.com>
2023-03-26 18:48:18 -0400

    Add clt.un, blt.un. Fix clt and blt

--- src/EazyDevirt/PatternMatching/Patterns/OpCodes/Comparators.cs
+++ src/EazyDevirt/PatternMatching/Patterns/OpCodes/Comparators.cs
@@ -12,6 +12,47 @@ internal record CltInnerPattern : IPattern
 {
     public IList<CilOpCode> Pattern => new List<CilOpCode>
     {
+        CilOpCodes.Ldarg_0,     // 63	00AA	ldarg.0
+        CilOpCodes.Castclass,   // 64	00AB	castclass	VMDoubleOperand
+        CilOpCodes.Callvirt,    // 65	00B0	callvirt	instance float64 VMDoubleOperand::method_3()
+        CilOpCodes.Ldarg_1,     // 66	00B5	ldarg.1
+        CilOpCodes.Castclass,   // 67	00B6	castclass	VMDoubleOperand
+        CilOpCodes.Callvirt,    // 68	00BB	callvirt	instance float64 VMDoubleOperand::method_3()
+        CilOpCodes.Clt,         // 69	00C0	clt
+        CilOpCodes.Stloc_0,     // 70	00C2	stloc.0
+    };
+
+    public bool MatchEntireBody => false;
+    
+    public bool InterchangeStlocOpCodes => true;
+}
+
+internal record Clt : IOpCodePattern
+{
+    public IList<CilOpCode> Pattern => new List<CilOpCode>
+    {
+        CilOpCodes.Call,        // 9	0011	call	bool VM::Clt_Inner(class VMOperandType, class VMOperandType)
+        CilOpCodes.Brtrue_S,    // 10	0016	brtrue.s	13 (001B) ldc.i4.1 
+        CilOpCodes.Ldc_I4_0,    // 11	0018	ldc.i4.0
+        CilOpCodes.Br_S,        // 12	0019	br.s	14 (001C) newobj instance void VMIntOperand::.ctor(int32)
+        CilOpCodes.Ldc_I4_1,    // 13	001B	ldc.i4.1
+    };
+
+    public CilOpCode? CilOpCode => CilOpCodes.Clt;
+
+    public bool MatchEntireBody => false;
+
+    public bool Verify(VMOpCode vmOpCode, int index = 0) => PatternMatcher.MatchesPattern(new CltInnerPattern(),
+        (vmOpCode.SerializedDelegateMethod.CilMethodBody!.Instructions[index].Operand as SerializedMethodDefinition)!);
+}
+#endregion Clt
+
+#region Clt_Un
+
+internal record Clt_UnInnerPattern : IPattern
+{
+    public IList<CilOpCode> Pattern => new List<CilOpCode>
+    {
         CilOpCodes.Ldloc_2,     // 71	00C2	ldloc.2
         CilOpCodes.Ldloc_3,     // 72	00C3	ldloc.3
         CilOpCodes.Blt_S,       // 73	00C4	blt.s	80 (00D6) ldc.i4.1 
@@ -28,22 +69,22 @@ internal record CltInnerPattern : IPattern
         "System.Boolean System.Double::IsNaN(System.Double)";
 }
 
-internal record Clt : IOpCodePattern
+internal record Clt_Un : IOpCodePattern
 {
     public IList<CilOpCode> Pattern => new List<CilOpCode>
     {
-        CilOpCodes.Call,        // 9	0011	call	bool VM::CltInner(class VMOperandType, class VMOperandType)
+        CilOpCodes.Call,        // 9	0011	call	bool VM::Clt_UnInner(class VMOperandType, class VMOperandType)
         CilOpCodes.Brtrue_S,    // 10	0016	brtrue.s	13 (001B) ldc.i4.1 
         CilOpCodes.Ldc_I4_0,    // 11	0018	ldc.i4.0
         CilOpCodes.Br_S,        // 12	0019	br.s	14 (001C) newobj instance void VMIntOperand::.ctor(int32)
         CilOpCodes.Ldc_I4_1,    // 13	001B	ldc.i4.1
     };
 
-    public CilOpCode? CilOpCode => CilOpCodes.Clt;
+    public CilOpCode? CilOpCode => CilOpCodes.Clt_Un;
 
     public bool MatchEntireBody => false;
 
-    public bool Verify(VMOpCode vmOpCode, int index = 0) => PatternMatcher.MatchesPattern(new CltInnerPattern(),
+    public bool Verify(VMOpCode vmOpCode, int index = 0) => PatternMatcher.MatchesPattern(new Clt_UnInnerPattern(),
         (vmOpCode.SerializedDelegateMethod.CilMethodBody!.Instructions[index].Operand as SerializedMethodDefinition)!);
 }
 #endregion Clt
