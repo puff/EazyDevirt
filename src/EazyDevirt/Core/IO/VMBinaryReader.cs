e9871d4243113b04ffbcd08db6dd01ad593946bb
puff <>
2023-04-24 14:43:19 -0400

    Add WIP ReadInt32 using emulation to VMBinaryReader

--- src/EazyDevirt/Core/IO/VMBinaryReader.cs
+++ src/EazyDevirt/Core/IO/VMBinaryReader.cs
@@ -1,14 +1,21 @@
-﻿using System.Text;
+﻿using System.Reflection.Metadata;
+using System.Text;
+using AsmResolver.PE.DotNet.Cil;
 using EazyDevirt.Core.Abstractions;
+using EazyDevirt.Devirtualization;
+using Echo.Platforms.AsmResolver.Emulation;
+using Echo.Platforms.AsmResolver.Emulation.Dispatch;
+using Echo.Platforms.AsmResolver.Emulation.Stack;
+using FieldDefinition = AsmResolver.DotNet.FieldDefinition;
+using MethodDefinition = AsmResolver.DotNet.MethodDefinition;
 
 namespace EazyDevirt.Core.IO;
 
-// TODO: The endianness is scrambled across samples. See issue #4
-// Add Echo emulation for the VMBinaryReader
 internal class VMBinaryReader : VMBinaryReaderBase
 {
     public VMBinaryReader(Stream input, bool leaveOpen = false) : base(input, Encoding.UTF8, leaveOpen)
-    { }
+    {
+    }
     
     public override sbyte ReadSByte()
     {
@@ -22,6 +29,75 @@ internal class VMBinaryReader : VMBinaryReaderBase
         return (bytes[2] << 8) | (bytes[3] << 16) | bytes[1] | (bytes[0] << 24);
     }
 
+    private static readonly CilCode[] AllowedCodes =
+    {
+        CilCode.Add, CilCode.Add_Ovf, CilCode.Add_Ovf_Un,
+        CilCode.Sub, CilCode.Sub_Ovf, CilCode.Sub_Ovf_Un,
+        CilCode.Mul, CilCode.Mul_Ovf, CilCode.Mul_Ovf_Un,
+
+        CilCode.Div, CilCode.Div_Un, CilCode.Rem, CilCode.Rem_Un,
+        CilCode.Xor, CilCode.And, CilCode.Or,
+
+        CilCode.Shl, CilCode.Shr, CilCode.Shr_Un,
+        CilCode.Ceq, CilCode.Cgt, CilCode.Cgt_Un, CilCode.Clt, CilCode.Clt_Un,
+
+        CilCode.Conv_I8,
+        CilCode.Ldelem_U1, CilCode.Ldfld
+    };
+    
+    private byte[] _buffer = Array.Empty<byte>();
+    
+    // TODO: DOES NOT WORK
+    public int ReadInt32Emulator()
+    {
+        var bytes = ReadBytes(4);
+        
+        var ctx = DevirtualizationContext.Instance;
+        var vm = new CilVirtualMachine(ctx.Module, ctx.Module.IsBit32Required);
+        
+        var method = ctx.Module.LookupMember<MethodDefinition>(0x0600027F);
+        
+        var instanceType = method.DeclaringType!.ToTypeSignature();
+        var isMemoryStreamField = ctx.Module.LookupMember<FieldDefinition>(0x040000C8);
+        var bufferField = ctx.Module.LookupMember<FieldDefinition>(0x040000C1);
+        
+        var instance = vm.ValueFactory.CreateNativeInteger(vm.Heap.AllocateObject(instanceType, true));
+        var instanceHandle = instance.AsObjectHandle(vm);
+
+        // var isMemoryStream = vm.ValueFactory.BitVectorPool.Rent(8, false);
+        // isMemoryStream.AsSpan().Write((byte)0); // if memorystream: 0 else: 1
+        // instanceHandle.WriteField(isMemoryStreamField, isMemoryStream);
+        
+        var actualBuffer = vm.Heap.AllocateSzArray(ctx.Module.CorLibTypeFactory.Byte, bytes.Length, false);
+        actualBuffer.AsObjectHandle(vm).WriteArrayData(bytes);
+        var buffer = vm.ValueFactory.CreateNativeInteger(actualBuffer);
+        buffer.AsSpan().WriteNativeInteger(actualBuffer, vm.Is32Bit);
+        instanceHandle.WriteField(bufferField, buffer);
+        
+        var resultI32 = 0;
+        vm.Dispatcher.BeforeInstructionDispatch += DispatcherOnBeforeInstructionDispatch;
+        vm.CallStack.Returned += delegate(object? _, CallEventArgs args)
+        {
+            var result = args.Frame.EvaluationStack.Pop(ctx.Module.CorLibTypeFactory.Int32);
+            resultI32 = result.AsSpan().I32;
+        };
+
+        // var result = vm.Call(method, new [] { instance });
+        vm.CallStack.Push(method).WriteArgument(0, instance);
+        vm.Run();
+
+        return resultI32;
+
+        return (bytes[2] << 8) | (bytes[3] << 16) | bytes[1] | (bytes[0] << 24);
+    }
+
+    private void DispatcherOnBeforeInstructionDispatch(object? _, CilDispatchEventArgs e)
+    {
+        var ins = e.Instruction;
+        if (!AllowedCodes.Contains(ins.OpCode.Code))
+            return;
+    }
+    
     public override int ReadInt32Special()
     {
         var bytes = ReadBytes(4);
