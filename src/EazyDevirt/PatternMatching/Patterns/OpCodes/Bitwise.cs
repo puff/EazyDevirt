b02053d384baee839e1c646653c9c98543d20df4
puff <33184334+puff@users.noreply.github.com>
2023-05-03 19:27:13 -0400

    Add shr, shr.un opcodes

--- src/EazyDevirt/PatternMatching/Patterns/OpCodes/Bitwise.cs
+++ src/EazyDevirt/PatternMatching/Patterns/OpCodes/Bitwise.cs
@@ -1,6 +1,6 @@
-﻿using AsmResolver.DotNet.Serialized;
+﻿using AsmResolver.DotNet.Code.Cil;
+using AsmResolver.DotNet.Serialized;
 using AsmResolver.PE.DotNet.Cil;
-using EazyDevirt.Core.Abstractions;
 using EazyDevirt.Core.Abstractions.Interfaces;
 using EazyDevirt.Core.Architecture;
 
@@ -50,3 +50,91 @@ internal record Xor : IOpCodePattern
     }
 }
 #endregion Xor
+
+#region Shr
+
+internal record ShrOperandsPattern : IPattern
+{
+    public IList<CilOpCode> Pattern => new List<CilOpCode>
+    {
+        CilOpCodes.Ldloc_1,     // 17	002E	ldloc.1
+        CilOpCodes.Ldc_I4_S,    // 18	002F	ldc.i4.s	0x1F
+        CilOpCodes.And,         // 19	0031	and
+        CilOpCodes.Shr,         // 20	0032	shr
+        CilOpCodes.Newobj,      // 21	0033	newobj	instance void VMIntOperand::.ctor(int32)
+        CilOpCodes.Ret,         // 22	0038	ret
+    };
+
+    public bool MatchEntireBody => false;
+    public bool InterchangeLdlocOpCodes => true;
+}
+
+internal record ShrOperandsHelperPattern : IPattern
+{
+    public IList<CilOpCode> Pattern => new List<CilOpCode>
+    {
+        CilOpCodes.Ldarg_0,     // 0	0000	ldarg.0
+        CilOpCodes.Call,        // 1	0001	call	instance class VMOperandType VM::PopStack()
+        CilOpCodes.Stloc_0,     // 2	0006	stloc.0
+        CilOpCodes.Ldarg_0,     // 3	0007	ldarg.0
+        CilOpCodes.Call,        // 4	0008	call	instance class VMOperandType VM::PopStack()
+        CilOpCodes.Stloc_1,     // 5	000D	stloc.1
+        CilOpCodes.Ldarg_0,     // 6	000E	ldarg.0
+        CilOpCodes.Ldloc_1,     // 7	000F	ldloc.1
+        CilOpCodes.Ldloc_0,     // 8	0010	ldloc.0
+        CilOpCodes.Ldarg_1,     // 9	0011	ldarg.1
+        CilOpCodes.Call,        // 10	0012	call	class VMOperandType VM::ShrOperands(class VMOperandType, class VMOperandType, bool)
+        CilOpCodes.Call,        // 11	0017	call	instance void VM::PushStack(class VMOperandType)
+        CilOpCodes.Ret          // 12	001C	ret
+    };
+
+    public bool Verify(CilInstructionCollection instructions, int index = 0)
+    {
+        var shrOperandsMethod = instructions[10].Operand as SerializedMethodDefinition;
+        
+        return PatternMatcher.MatchesPattern(new ShrOperandsPattern(), shrOperandsMethod!);
+    }
+}
+
+internal record Shr : IOpCodePattern
+{
+    public IList<CilOpCode> Pattern => new List<CilOpCode>
+    {
+        CilOpCodes.Ldarg_0,     // 0	0000	ldarg.0
+        CilOpCodes.Ldc_I4_0,    // 1	0001	ldc.i4.0
+        CilOpCodes.Callvirt,    // 2	0002	callvirt	instance void VM::ShrOperandsHelper(bool)
+        CilOpCodes.Ret          // 3	0007	ret
+    };
+
+    public CilOpCode? CilOpCode => CilOpCodes.Shr;
+
+    public bool Verify(VMOpCode vmOpCode, int index = 0)
+    {
+        var shrOperandsHelperMethod =
+            vmOpCode.SerializedDelegateMethod.CilMethodBody!.Instructions[2].Operand as SerializedMethodDefinition;
+
+        return PatternMatcher.MatchesPattern(new ShrOperandsHelperPattern(), shrOperandsHelperMethod!);
+    }
+}
+
+internal record Shr_Un : IOpCodePattern
+{
+    public IList<CilOpCode> Pattern => new List<CilOpCode>
+    {
+        CilOpCodes.Ldarg_0,     // 0	0000	ldarg.0
+        CilOpCodes.Ldc_I4_1,    // 1	0001	ldc.i4.1
+        CilOpCodes.Callvirt,    // 2	0002	callvirt	instance void VM::ShrOperandsHelper(bool)
+        CilOpCodes.Ret          // 3	0007	ret
+    };
+
+    public CilOpCode? CilOpCode => CilOpCodes.Shr_Un;
+
+    public bool Verify(VMOpCode vmOpCode, int index = 0)
+    {
+        var shrOperandsHelperMethod =
+            vmOpCode.SerializedDelegateMethod.CilMethodBody!.Instructions[2].Operand as SerializedMethodDefinition;
+
+        return PatternMatcher.MatchesPattern(new ShrOperandsHelperPattern(), shrOperandsHelperMethod!);
+    }
+}
+#endregion Shr
