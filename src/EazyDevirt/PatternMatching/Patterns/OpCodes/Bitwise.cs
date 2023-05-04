be22a093654505d298fe5554e3352be1822f216c
puff <33184334+puff@users.noreply.github.com>
2023-05-03 21:44:43 -0400

    Add or opcode

--- src/EazyDevirt/PatternMatching/Patterns/OpCodes/Bitwise.cs
+++ src/EazyDevirt/PatternMatching/Patterns/OpCodes/Bitwise.cs
@@ -6,6 +6,61 @@ using EazyDevirt.Core.Architecture;
 
 namespace EazyDevirt.PatternMatching.Patterns.OpCodes;
 
+#region Or
+
+internal record OrOperandsPattern : IPattern
+{
+    public IList<CilOpCode> Pattern => new List<CilOpCode>
+    {
+        CilOpCodes.Ldarg_1,     // 8	0017	ldarg.1
+        CilOpCodes.Castclass,   // 9	0018	castclass	VMIntOperand
+        CilOpCodes.Callvirt,    // 10	001D	callvirt	instance int32 VMIntOperand::method_3()
+        CilOpCodes.Ldarg_2,     // 11	0022	ldarg.2
+        CilOpCodes.Castclass,   // 12	0023	castclass	VMIntOperand
+        CilOpCodes.Callvirt,    // 13	0028	callvirt	instance int32 VMIntOperand::method_3()
+        CilOpCodes.Stloc_S,     // 14	002D	stloc.s	V_6 (6)
+        CilOpCodes.Ldloc_S,     // 15	002F	ldloc.s	V_6 (6)
+        CilOpCodes.Or,          // 16	0031	or
+        CilOpCodes.Newobj,      // 17	0032	newobj	instance void VMIntOperand::.ctor(int32)
+        CilOpCodes.Ret,         // 18	0037	ret
+    };
+
+    public bool MatchEntireBody => false;
+    public bool InterchangeLdlocOpCodes => true;
+    public bool InterchangeStlocOpCodes => true;
+}
+
+internal record Or : IOpCodePattern
+{
+    public IList<CilOpCode> Pattern => new List<CilOpCode>
+    {
+        CilOpCodes.Ldarg_0,     // 0	0000	ldarg.0
+        CilOpCodes.Callvirt,    // 1	0001	callvirt	instance class VMOperandType VM::PopStack()
+        CilOpCodes.Stloc_0,     // 2	0006	stloc.0
+        CilOpCodes.Ldarg_0,     // 3	0007	ldarg.0
+        CilOpCodes.Callvirt,    // 4	0008	callvirt	instance class VMOperandType VM::PopStack()
+        CilOpCodes.Stloc_1,     // 5	000D	stloc.1
+        CilOpCodes.Ldarg_0,     // 6	000E	ldarg.0
+        CilOpCodes.Ldarg_0,     // 7	000F	ldarg.0
+        CilOpCodes.Ldloc_1,     // 8	0010	ldloc.1
+        CilOpCodes.Ldloc_0,     // 9	0011	ldloc.0
+        CilOpCodes.Callvirt,    // 10	0012	callvirt	instance class VMOperandType VM::OrOperands(class VMOperandType, class VMOperandType)
+        CilOpCodes.Callvirt,    // 11	0017	callvirt	instance void VM::PushStack(class VMOperandType)
+        CilOpCodes.Ret          // 12	001C	ret
+    };
+
+    public CilOpCode? CilOpCode => CilOpCodes.Or;
+
+    public bool Verify(VMOpCode vmOpCode, int index = 0)
+    {
+        var orOperandsHelperMethod =
+            vmOpCode.SerializedDelegateMethod.CilMethodBody!.Instructions[10].Operand as SerializedMethodDefinition;
+
+        return PatternMatcher.MatchesPattern(new OrOperandsPattern(), orOperandsHelperMethod!);
+    }
+}
+#endregion Or
+
 #region Xor
 internal record XorOperatorPattern : IPattern
 {
